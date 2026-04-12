e558a77f8d1774c93f4f672de0dd6a0095b2ad1c
blake <blake.hourigan@atlasied.com>
2026-04-11 20:58:06 -0400

    install roslyn nvim plugin via vim.pack

--- nvim/lua/blake/vim-plugins/init.lua
+++ nvim/lua/blake/vim-plugins/init.lua
@@ -17,8 +17,9 @@ vim.pack.add({
     -------------------------------------------------------------------
     { src = "https://github.com/mason-org/mason.nvim", version = 'main'}, 
     -------------------------------------------------------------------
-    { src = "https://github.com/neovim/nvim-lspconfig", version = 'master' }
+    { src = "https://github.com/neovim/nvim-lspconfig", version = 'master' },
     -------------------------------------------------------------------
+    { src = "https://github.com/seblyng/roslyn.nvim", version = 'main' }
 })
 
 -- add builtin packages to this session 
