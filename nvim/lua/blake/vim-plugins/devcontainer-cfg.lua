3b259a099fe1dd632744e3175e220e59940ab9ec
Blake <houriganblake@protonmail.com>
2026-04-19 12:56:44 -0400

    Change theme & add devcontainer plug

---
+++ nvim/lua/blake/vim-plugins/devcontainer-cfg.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>Du", ":DevcontainerUp<CR>")
+vim.keymap.set("n", "<leader>Dc", ":DevcontainerConnect<CR>")
--- nvim/lua/blake/vim-plugins/init.lua
+++ nvim/lua/blake/vim-plugins/init.lua
@@ -3,38 +3,42 @@
 -- consider coverage
 
 vim.pack.add({
-    { src = "https://github.com/folke/tokyonight.nvim", version = 'main' },
+    { src = "https://github.com/catppuccin/nvim",                          name = "main" },
     -------------------------------------------------------------------
-    { src = "https://github.com/nvim-lua/plenary.nvim", version = 'master'},
-    { src = "https://github.com/ThePrimeagen/harpoon", version = 'harpoon2'},
+    { src = "https://github.com/nvim-lua/plenary.nvim",                    version = 'master' },
+    { src = "https://github.com/ThePrimeagen/harpoon",                     version = 'harpoon2' },
     -------------------------------------------------------------------
-    { src = "https://github.com/nvzone/volt", version = 'main'}, 
-    { src = "https://github.com/nvzone/typr", version = 'main'},
+    { src = "https://github.com/nvzone/volt",                              version = 'main' },
+    { src = "https://github.com/nvzone/typr",                              version = 'main' },
     -------------------------------------------------------------------
-    { src = "https://github.com/nvim-telescope/telescope.nvim", version = 'master'}, 
-    { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", version = 'main'},
-    { src = "https://github.com/nvim-lua/plenary.nvim", version = 'master'},
+    { src = "https://github.com/nvim-lua/plenary.nvim",                    version = 'master' },
+    { src = "https://github.com/nvim-telescope/telescope.nvim",            version = 'master' },
+    { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", version = 'main' },
     -------------------------------------------------------------------
-    { src = "https://github.com/neogitorg/neogit", version = 'master'}, 
-    { src = "https://github.com/nvim-lua/plenary.nvim", version = 'master'},
-    { src = "https://github.com/sindrets/diffview.nvim", version = 'main'},
+    { src = "https://github.com/neogitorg/neogit",                         version = 'master' },
+    { src = "https://github.com/nvim-lua/plenary.nvim",                    version = 'master' },
+    { src = "https://github.com/sindrets/diffview.nvim",                   version = 'main' },
     -------------------------------------------------------------------
-    { src = "https://github.com/mason-org/mason.nvim", version = 'main'}, 
+    { src = "https://github.com/mason-org/mason.nvim",                     version = 'main' },
     -------------------------------------------------------------------
-    { src = "https://github.com/neovim/nvim-lspconfig", version = 'master' },
+    { src = "https://github.com/neovim/nvim-lspconfig",                    version = 'master' },
     -------------------------------------------------------------------
-    { src = "https://github.com/seblyng/roslyn.nvim", version = 'main' },
+    { src = "https://github.com/seblyng/roslyn.nvim",                      version = 'main' },
     -------------------------------------------------------------------
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter",          version = 'main' },
+    -------------------------------------------------------------------
+    { src = "https://github.com/akinsho/toggleterm.nvim",                  version = 'main' },
+    { src = "https://github.com/erichlf/devcontainer-cli.nvim",            version = 'main' },
+
 })
 
--- add builtin packages to this session 
+-- add builtin packages to this session
 vim.cmd("packadd nvim.undotree")
 
 -- activate the colorscheme
-vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme catppuccin-nvim")
 
--- setups 
+-- setups
 require("harpoon").setup()
 
 require("telescope").setup({
@@ -47,29 +51,43 @@ require("telescope").setup({
 
 require("mason").setup(
     {
-
         registries = {
             "github:Crashdummyy/mason-registry", -- The Roslyn/C# registry
-            "github:mason-org/mason-registry",   -- The default registry 
+            "github:mason-org/mason-registry",   -- The default registry
         },
     }
 )
 
-local ts_parsers = { 
-    "c", "lua", "rust", "python", "c_sharp", 
-    "yaml",     
+require("toggleterm").setup()
+
+require("devcontainer-cli").setup(
+    {
+        interactive = false,
+        toplevel = true,
+        remove_existing_container = true,
+        shell = "bash",
+        nvim_binary = "nvim",
+        log_level = "debug",
+        console_level = "info"
+    }
+)
+
+local ts_parsers = {
+    "c", "lua", "rust", "python", "c_sharp",
+    "yaml",
     "bash",
-    "vim", "vimdoc", 
-    "markdown", 
+    "vim", "vimdoc",
+    "markdown",
 }
 
 local nts = require("nvim-treesitter")
 nts.install(ts_parsers)
 
 -- my config of the plugins
-require("blake.vim-plugins.harpoon")
+require("blake.vim-plugins.harpoon-cfg")
 require("blake.vim-plugins.Typr")
 require("blake.vim-plugins.undotree")
-require("blake.vim-plugins.telescope")
+require("blake.vim-plugins.telescope-cfg")
 require("blake.vim-plugins.neogit")
 require("blake.vim-plugins.lsp")
+require("blake.vim-plugins.devcontainer-cfg")
